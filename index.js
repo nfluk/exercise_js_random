b2f1475b4fab39f5d2f732b055dd9b87b3e086b5
nfluk <nickylukic@gmail.com>
2022-05-05 21:59:10 +0200

    switch ... case

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ const hour = 4;
 if (hour >= 6 && hour < 12) console.log("Good morning!");
 else if (hour >= 12 && hour < 18) console.log("Good afternoon!");
 else console.log("Good evening!");
+
+// Practice switch ... case
+
+let role;
+
+switch (role) {
+  case "guest":
+    console.log("Guest User");
+    break;
+
+  case "moderator":
+    console.log("Moderator User");
+    break;
+
+  default:
+    console.log("Unknown User");
+}
