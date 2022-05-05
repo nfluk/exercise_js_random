9b5b509b37e0d8664d89cccebc2d62a509123cad
nfluk <nickylukic@gmail.com>
2022-05-05 21:52:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Practice if ... else
+// Hour
+// If hour is between 6am and 12pm: Good morning!
+// If it is between 12pm and 6pm: Good afternoon!
+// Otherwise: Good evening!
+
+const hour = 4;
+
+if (hour >= 6 && hour < 12) console.log("Good morning!");
+else if (hour >= 12 && hour < 18) console.log("Good afternoon!");
+else console.log("Good evening!");
