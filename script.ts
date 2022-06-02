50356144fb944ef4b1e15d94b9d1348bdcbff7f2
nfluk <nickylukic@gmail.com>
2022-06-02 08:03:11 +0200

    added evolution and comments

--- script.ts
+++ script.ts
@@ -1,5 +1,15 @@
-const pow = (x:number, y:number):string => {
+// specify that in the params that the arguments should be numbers
+const pow1 = (x:number, y:number) => {
+  return Math.pow(x, y);
+}
+
+pow1(4, 9)
+
+
+
+// specify a string as a return value type
+const pow2 = (x:number, y:number):string => {
   return Math.pow(x, y).toString();
 }
 
-pow(4, 9)
+pow2(4, 9)
